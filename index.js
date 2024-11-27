bb3e63521d569bad855fb2d22b86405e3683523d
Daisy Jimenez <jimenez.daisy9494@gmail.com>
2024-11-27 12:05:03 -0800

    array lab done

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop("")
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("")
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats]
+    copyOfCats.push("Broom")
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = [...cats]
+    copyOfCats.unshift("Arnold")
+    return copyOfCats
+}
+
+function removeLastCat(name) {
+    const copyOfCats = [...cats]
+    copyOfCats.pop("")
+    return copyOfCats
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = [...cats]
+    copyOfCats.shift("")
+    return copyOfCats
+}
+
+
+
